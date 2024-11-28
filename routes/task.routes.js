ebbf48367e521a726ede0eacc1551f65a93eaeab
Gonzalo Barroso <gonzalobarroso50@gmail.com>
2024-11-27 21:26:38 -0300

    lista con datos mock

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const tasks = require('../task.json');
 
-// Obtener todas las tareas
+// Obtener todas las tareas 
 router.get('/', (req, res) => {
     res.status(200).json(tasks);
 });
